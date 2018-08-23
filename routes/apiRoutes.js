3a2bca0159b531e52d30fb93662f44eb467fd398
Raymond <rs01247@gmail.com>
2018-08-23 11:36:18 -0700

    scrape on apiroute.js created

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,24 @@
 const express = require('express');
+const cheerio = require('cheerio');
+const axios = require('axios');
+
 const router = express.Router();
+
+// GET ROUTE FOR SCRAPING REDDIT
+router.get('/scrape', function (req, res) {
+    axios.get('https://www.old.reddit.com/')
+    .then(function (resp){
+
+        // LOAD RESPONSE INTO CHEERIO AND SAVE AS '$'
+        const $ = cheerio.load(resp.data); 
+
+        // GRAB EVERY DIV W/ 'spacer' TAG
+        $('spacer div').each(function(i, elem) {
+            const result = {};
+
+
+        })
+    })
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,47 @@
+// REQUIRE NPM
 const express = require('express')
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const logger = require('morgan');
 const cheerio = require('cheerio');
 const mongoose = require('mongoose');
 const request = require('request');
+
+// ROUTES AND MODELS
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
+const db = require('./models')
 
-const app = express();
+const app = express();  
 const PORT = process.env.PORT || 8080;
 
-mongoose.connect('mongodb://localhost/my_database');
+// WHEN DEPLOYED, USE DEPLOYED DB, OTHERWISE USE LOCAL mongoHeadlines DB
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+// ACCESS TO PUBLIC FOLDER
 app.use(express.static(__dirname + '/public'));
 
+// MORGAN LOGGER
+app.use(logger("dev"));
+
+// BODY PARSER
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//HANDLEBARS
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");;
 
+// USE ROUTES
 app.use(htmlRoutes);
 app.use(apiRoutes);
 
+// ES6 PROMISES ON MONGOOSE
+mongoose.Promise = Promise;
+// CONNECT TO MONGO DB
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true
+});
 
 app.listen(PORT, function () {
     console.log(`Listening on ${PORT}`)
